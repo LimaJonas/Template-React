ade5ddf7cb2df7cec56ec177086dc25b5bf5bca8
Jonas Lima <jonaslima015@gmail.com>
2020-12-30 19:14:40 -0300

    Navbar e header separados em componentes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
+import React, { Component } from 'react';
+
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+
 function App() {
   return (
     <div classNameName='App'>
-    {/* Navigation */}
-        <nav className='navbar navbar-expand-lg bg-secondary text-uppercase fixed-top' id='mainNav'>
-            <div className='container'>
-                <a className='navbar-brand js-scroll-trigger' href='#page-top'>Start Bootstrap</a>
-                <button className='navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
-                    Menu
-                    <i className='fas fa-bars'></i>
-                </button>
-                <div className='collapse navbar-collapse' id='navbarResponsive'>
-                    <ul className='navbar-nav ml-auto'>
-                        <li className='nav-item mx-0 mx-lg-1'><a className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger' href='#portfolio'>Portfolio</a></li>
-                        <li className='nav-item mx-0 mx-lg-1'><a className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger' href='#about'>About</a></li>
-                        <li className='nav-item mx-0 mx-lg-1'><a className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger' href='#contact'>Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        {/* Masthead*/}
-        <header className='masthead bg-primary text-white text-center'>
-            <div className='container d-flex align-items-center flex-column'>
-                {/* Masthead Avatar Image*/}
-                <img className='masthead-avatar mb-5' src='assets/img/avataaars.svg' alt='' />
-                {/* Masthead Heading*/}
-                <h1 className='masthead-heading text-uppercase mb-0'>Start Bootstrap</h1>
-                {/* Icon Divider*/}
-                <div className='divider-custom divider-light'>
-                    <div className='divider-custom-line'></div>
-                    <div className='divider-custom-icon'><i className='fas fa-star'></i></div>
-                    <div className='divider-custom-line'></div>
-                </div>
-                {/* Masthead Subheading*/}
-                <p className='masthead-subheading font-weight-light mb-0'>Graphic Artist - Web Designer - Illustrator</p>
-            </div>
-        </header>
+        <Navbar />
+        <Header />
+        
         {/* Portfolio Section*/}
         <section className='page-section portfolio' id='portfolio'>
             <div className='container'>
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Header = props => {
+    return(
+        
+        <header className='masthead bg-primary text-white text-center'>
+            <div className='container d-flex align-items-center flex-column'>
+                {/* Masthead Avatar Image*/}
+                <img className='masthead-avatar mb-5' src='assets/img/avataaars.svg' alt='' />
+                {/* Masthead Heading*/}
+                <h1 className='masthead-heading text-uppercase mb-0'>Start Bootstrap</h1>
+                {/* Icon Divider*/}
+                <div className='divider-custom divider-light'>
+                    <div className='divider-custom-line'></div>
+                    <div className='divider-custom-icon'><i className='fas fa-star'></i></div>
+                    <div className='divider-custom-line'></div>
+                </div>
+                {/* Masthead Subheading*/}
+                <p className='masthead-subheading font-weight-light mb-0'>Graphic Artist - Web Designer - Illustrator</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
