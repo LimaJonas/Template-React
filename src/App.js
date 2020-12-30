1e450b4cd128b8b6dc705e94074f6d1c72ad8ac0
Jonas Lima <jonaslima015@gmail.com>
2020-12-30 19:18:50 -0300

    sessão About e Portfolio separados em componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+// Conteúdos do site
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import About from './components/About';
 
 
 function App() {
@@ -9,102 +12,10 @@ function App() {
     <div classNameName='App'>
         <Navbar />
         <Header />
+        <Portfolio />
+        <About />
         
-        {/* Portfolio Section*/}
-        <section className='page-section portfolio' id='portfolio'>
-            <div className='container'>
-                {/* Portfolio Section Heading*/}
-                <h2 className='page-section-heading text-center text-uppercase text-secondary mb-0'>Portfolio</h2>
-                {/* Icon Divider*/}
-                <div className='divider-custom'>
-                    <div className='divider-custom-line'></div>
-                    <div className='divider-custom-icon'><i className='fas fa-star'></i></div>
-                    <div className='divider-custom-line'></div>
-                </div>
-                {/* Portfolio Grid Items*/}
-                <div className='row justify-content-center'>
-                    {/* Portfolio Item 1*/}
-                    <div className='col-md-6 col-lg-4 mb-5'>
-                        <div className='portfolio-item mx-auto' data-toggle='modal' data-target='#portfolioModal1'>
-                            <div className='portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100'>
-                                <div className='portfolio-item-caption-content text-center text-white'><i className='fas fa-plus fa-3x'></i></div>
-                            </div>
-                            <img className='img-fluid' src='assets/img/portfolio/cabin.png' alt='' />
-                        </div>
-                    </div>
-                    {/* Portfolio Item 2*/}
-                    <div className='col-md-6 col-lg-4 mb-5'>
-                        <div className='portfolio-item mx-auto' data-toggle='modal' data-target='#portfolioModal2'>
-                            <div className='portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100'>
-                                <div className='portfolio-item-caption-content text-center text-white'><i className='fas fa-plus fa-3x'></i></div>
-                            </div>
-                            <img className='img-fluid' src='assets/img/portfolio/cake.png' alt='' />
-                        </div>
-                    </div>
-                    {/* Portfolio Item 3*/}
-                    <div className='col-md-6 col-lg-4 mb-5'>
-                        <div className='portfolio-item mx-auto' data-toggle='modal' data-target='#portfolioModal3'>
-                            <div className='portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100'>
-                                <div className='portfolio-item-caption-content text-center text-white'><i className='fas fa-plus fa-3x'></i></div>
-                            </div>
-                            <img className='img-fluid' src='assets/img/portfolio/circus.png' alt='' />
-                        </div>
-                    </div>
-                    {/* Portfolio Item 4*/}
-                    <div className='col-md-6 col-lg-4 mb-5 mb-lg-0'>
-                        <div className='portfolio-item mx-auto' data-toggle='modal' data-target='#portfolioModal4'>
-                            <div className='portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100'>
-                                <div className='portfolio-item-caption-content text-center text-white'><i className='fas fa-plus fa-3x'></i></div>
-                            </div>
-                            <img className='img-fluid' src='assets/img/portfolio/game.png' alt='' />
-                        </div>
-                    </div>
-                    {/* Portfolio Item 5*/}
-                    <div className='col-md-6 col-lg-4 mb-5 mb-md-0'>
-                        <div className='portfolio-item mx-auto' data-toggle='modal' data-target='#portfolioModal5'>
-                            <div className='portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100'>
-                                <div className='portfolio-item-caption-content text-center text-white'><i className='fas fa-plus fa-3x'></i></div>
-                            </div>
-                            <img className='img-fluid' src='assets/img/portfolio/safe.png' alt='' />
-                        </div>
-                    </div>
-                    {/* Portfolio Item 6*/}
-                    <div className='col-md-6 col-lg-4'>
-                        <div className='portfolio-item mx-auto' data-toggle='modal' data-target='#portfolioModal6'>
-                            <div className='portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100'>
-                                <div className='portfolio-item-caption-content text-center text-white'><i className='fas fa-plus fa-3x'></i></div>
-                            </div>
-                            <img className='img-fluid' src='assets/img/portfolio/submarine.png' alt='' />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        {/* About Section*/}
-        <section className='page-section bg-primary text-white mb-0' id='about'>
-            <div className='container'>
-                {/* About Section Heading*/}
-                <h2 className='page-section-heading text-center text-uppercase text-white'>About</h2>
-                {/* Icon Divider*/}
-                <div className='divider-custom divider-light'>
-                    <div className='divider-custom-line'></div>
-                    <div className='divider-custom-icon'><i className='fas fa-star'></i></div>
-                    <div className='divider-custom-line'></div>
-                </div>
-                {/* About Section Content*/}
-                <div className='row'>
-                    <div className='col-lg-4 ml-auto'><p className='lead'>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
-                    <div className='col-lg-4 mr-auto'><p className='lead'>You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
-                </div>
-                {/* About Section Button*/}
-                <div className='text-center mt-4'>
-                    <a className='btn btn-xl btn-outline-light' href='https://startbootstrap.com/theme/freelancer/'>
-                        <i className='fas fa-download mr-2'></i>
-                        Free Download!
-                    </a>
-                </div>
-            </div>
-        </section>
+        
         {/* Contact Section*/}
         <section className='page-section' id='contact'>
             <div className='container'>
